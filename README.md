c21d5a5eb85e70b53b80b433c4a2e94afa3e98e0
zaqueu lima dos santos <zaqueulima1@gmail.com>
2019-05-21 18:06:31 -0300

    Create spotify app

---
+++ README.md
@@ -0,0 +1,1 @@
+## Sopotify clone react-native
---
+++ src/store/ducks/player.js
@@ -0,0 +1,44 @@
+import { createReducer, createActions } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
+
+/**
+ * Actions Types & Cretors
+ */
+const { Types, Creators } = createActions({
+  setPodcastRequest: ['podcast', 'episodeId'],
+  setPodcastSuccess: ['podcast'],
+  setCurrent: ['id'],
+  play: null,
+  pause: null,
+  next: null,
+  prev: null,
+  reset: null,
+});
+
+export const PlayerTypes = Types;
+export default Creators;
+
+/**
+ * Initial state
+ */
+
+export const INITIAL_STATE = Immutable({
+  podcast: null,
+  current: null,
+  playing: false,
+});
+
+/**
+ * Reducer
+ */
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.SET_PODCAST_SUCCESS]: (state, { podcast }) => state.merge({
+    podcast,
+    current: podcast.tracks[0].id,
+  }),
+  [Types.SET_CURRENT]: (state, { id }) => state.merge({ current: id }),
+  [Types.PLAY]: state => state.merge({ playing: true }),
+  [Types.PAUSE]: state => state.merge({ playing: false }),
+  [Types.RESET]: state => state.merge({ podcast: null, current: null, playing: false }),
+});
